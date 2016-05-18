1deb8c3c80167107beff2eb99e13491ee63f9094
Thwipshot <ethumpert@gmail.com>
2016-05-18 14:26:53 -0500

    solved 2 of the three functions

--- variadic.js
+++ variadic.js
@@ -1,24 +1,31 @@
 //This function should sum all of the arguments.
 function sum() {
   //complete
+  var args = Array.prototype.slice.call(arguments);
+  return args.reduce(function(a, v){
+    return a + v;
+  }, 0);
 }
 
-sum(1, 2, 5, 10); // -> 18
-sum(1, 5); // -> 6
+console.log(sum(1, 2, 5, 10)); // -> 18
+console.log(sum(1, 5)); // -> 6
 
 
 //This function needs to use the first argument as the delimiter and should create a concatenated string.
 function concat() {
   //complete
+  var args = Array.prototype.slice.call(arguments);
+  var delimitter = args.shift();
+  return args.join(delimitter);
 }
-
-concat('+', 'This', 'is', 'several', 'values'); // -> This+is+several+values
+console.log(concat('+', 'This', 'is', 'several', 'values')); // -> This+is+several+values
 
 
 
 //This function needs to find the max of all the values passed in.
 function max() {
   //complete
+  
 }
 
 max(1, 7, 65, 8); // -> 65
