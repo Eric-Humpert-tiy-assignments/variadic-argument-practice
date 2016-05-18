3060c936dca2571bb10eb1850e86649fc3e0322b
Thwipshot <ethumpert@gmail.com>
2016-05-18 13:13:27 -0500

    initial file creation

---
+++ variadic.js
@@ -0,0 +1,24 @@
+//This function should sum all of the arguments.
+function sum() {
+  //complete
+}
+
+sum(1, 2, 5, 10); // -> 18
+sum(1, 5); // -> 6
+
+
+//This function needs to use the first argument as the delimiter and should create a concatenated string.
+function concat() {
+  //complete
+}
+
+concat('+', 'This', 'is', 'several', 'values'); // -> This+is+several+values
+
+
+
+//This function needs to find the max of all the values passed in.
+function max() {
+  //complete
+}
+
+max(1, 7, 65, 8); // -> 65
